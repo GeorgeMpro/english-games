036950ad9893df84f0ff97c86e369f3911fb6c94
george <George.mpro87@gmail.com>
2025-05-02 09:42:59 +0300

    Added routing Added wiki images extraction Basic colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {AgeCategoryComponent} from './age-category/age-category.component';
+import {HomeComponent} from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AgeCategoryComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+import {HomeComponent} from './home/home.component';
+import {KidsComponent} from './age-category/kids/kids.component';
+import {MatchWordsGameComponent} from './age-category/kids/match-words-game/match-words-game.component';
+import {MatchSoundsGameComponent} from './age-category/kids/match-sounds-game/match-sounds-game.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    data: {label: 'Home Page'}
+  },
+  {
+    path: 'kids',
+    component: KidsComponent,
+    data: {label: 'Kids Page'},
+    children: [
+      {path: 'match-words', component: MatchWordsGameComponent, data: {label: 'Match Words Game'}},
+      {path: 'match-sounds', component: MatchSoundsGameComponent, data: {label: 'Match Sounds Game'}},
+    ]
+  },
+  // todo
+  // { path: 'young-adults', component: YoungAdultsComponent },
+  // { path: 'adults',      component: AdultsComponent },
+  // { path: 'professionals', component: ProfessionalsComponent },
+  {path: '**', redirectTo: ''}
+];
+
