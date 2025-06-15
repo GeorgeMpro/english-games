a6054314f6d893aaabfeb8f42eb9c9f5c5b834d1
george <George.mpro87@gmail.com>
2025-06-15 20:47:11 +0300

    Fixed Wiki Queries

--- src/app/shared/services/wiki.service.ts
+++ src/app/shared/services/wiki.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {forkJoin, Observable} from 'rxjs';
 import {MatchItem, WikiPage, WikiQueryResponse} from '../models/kids.models';
 import {map} from 'rxjs/operators';
 import {HttpClient, HttpParams} from '@angular/common/http';
@@ -16,15 +16,36 @@ export class WikiService {
   }
 
   // todo cleanup and add doc
-  getItems(words: string[]): Observable<MatchItem[]> {
-    const params = this.buildParams(words);
-    return this.http.get<WikiQueryResponse>(this.apiUrl, {params}).pipe(
-      map(response => this.mapPagesToItems(response.query.pages)),
-      // todo notice: filters items without images without testing
-      map(items => items.filter(item => !!item.imageUrl)),
+  // getItems(words: string[]): Observable<MatchItem[]> {
+  //   const params = this.buildParams(words);
+  //   return this.http.get<WikiQueryResponse>(this.apiUrl, {params}).pipe(
+  //     map(response => this.mapPagesToItems(response.query.pages)),
+  //     // todo notice: filters items without images without testing
+  //     map(items => items.filter(item => !!item.imageUrl)),
+  //
+  //     // todo keep or remove?
+  //     // catchError(() => of([]))
+  //   );
+  // }
 
-      // todo keep or remove?
-      // catchError(() => of([]))
+  getItems(words: string[]): Observable<MatchItem[]> {
+    // Notice: silent fail when long queries to wikipedia
+    const batchSize = 25;
+    const batches: string[][] = [];
+
+    for (let i = 0; i < words.length; i += batchSize) {
+      batches.push(words.slice(i, i + batchSize));
+    }
+
+    return forkJoin(
+      batches.map(batch => this.http.get<WikiQueryResponse>(this.apiUrl, {
+        params: this.buildParams(batch)
+      }).pipe(
+        map(res => this.mapPagesToItems(res.query.pages)),
+        map(items => items.filter(item => !!item.imageUrl))
+      ))
+    ).pipe(
+      map(batchResults => batchResults.flat())
     );
   }
 
