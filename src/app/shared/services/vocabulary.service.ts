ccc7e3366e238e37333ece0fb93cc2a0ef50cf58
george <George.mpro87@gmail.com>
2025-06-17 09:35:38 +0300

    Add new categories and corresponding data files for nature, travel, sports, and city

--- src/app/shared/services/vocabulary.service.ts
+++ src/app/shared/services/vocabulary.service.ts
@@ -7,6 +7,10 @@ import utensils from '../../../assets/data/utensils.json';
 import scenery from '../../../assets/data/scenery.json'
 import food from '../../../assets/data/food.json'
 import clothes from '../../../assets/data/clothes.json'
+import nature from '../../../assets/data/nature.json'
+import travel from '../../../assets/data/travel-and-transport.json'
+import sport from '../../../assets/data/sports-and-fitness.json'
+import city from '../../../assets/data/city-and-architecture.json'
 
 export enum Category {
   Animals = 'animals',
@@ -15,6 +19,10 @@ export enum Category {
   Scenery = 'scenery',
   Food = 'food',
   Clothes = 'clothes',
+  Nature = 'nature',
+  Travel = 'travel',
+  Sport = 'sport',
+  City = 'city',
 }
 
 @Injectable({
@@ -29,6 +37,10 @@ export class VocabularyService {
     [Category.Scenery]: scenery,
     [Category.Food]: food,
     [Category.Clothes]: clothes,
+    [Category.Nature]: nature,
+    [Category.Travel]: travel,
+    [Category.Sport]: sport,
+    [Category.City]: city
   };
 
   constructor() {
