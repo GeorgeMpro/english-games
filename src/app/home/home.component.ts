71ca60da0d6e2cc56799abd193d52fd290e2aa76
George <george.mpro87@gmail.com>
2025-07-15 20:25:14 +0300

    done home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,8 +6,16 @@ import {AgeCategoryComponent} from '../age-category/age-category.component';
   imports: [
     AgeCategoryComponent
   ],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  template: `
+    <main class="content">
+      <section class="hero">
+        <h1 class="hero-title">{{ title }}</h1>
+        <p class="hero-subtitle">Choose your learning level:</p>
+      </section>
+      <app-age-category></app-age-category>
+    </main>
+  `,
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   title = 'Start Your English Adventure';
